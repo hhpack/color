876c90ec694bc01f4d84c96c1d0e336516ce63fc
holyshared <holy.shared.design@gmail.com>
2015-11-02 12:59:20 +0900

    add sources

---
+++ src/Color.hh
@@ -0,0 +1,51 @@
+<?hh //strict
+
+/**
+ * This file is part of hhpack\color.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace hhpack\color;
+
+final class Color
+{
+
+    public function __construct(
+        private string $text,
+        private ForegroundColor $color = ForegroundColor::White
+    )
+    {
+    }
+
+    public function color(ForegroundColor $color) : this
+    {
+        $this->color = $color;
+        return $this;
+    }
+
+    public function text(string $text) : this
+    {
+        $this->text = $text;
+        return $this;
+    }
+
+    public function display() : this
+    {
+        fwrite(STDOUT, (string) $this);
+    }
+
+    public static function fromText(string $text) : this
+    {
+        return new Color($text);
+    }
+
+    public function __toString() : string
+    {
+        return "\e[" . (string) $this->color . "m" . $this->text . "\e[0m";
+    }
+
+}
---
+++ src/ForegroundColor.hh
@@ -0,0 +1,32 @@
+<?hh //strict
+
+/**
+ * This file is part of hhpack\color.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace hhpack\color;
+
+enum ForegroundColor : string
+{
+    Black = '0;30';
+    Red = '0;31';
+    Green = '0;32';
+    Yellow = '0;33';
+    Blue = '0;34';
+    Magenta = '0;35';
+    Cyan = '0;36';
+    White = '0;37';
+    Gray = '1;30';
+    LightRed = '1;31';
+    LightGreen = '1;32';
+    LightYellow = '1;33';
+    LightBlue = '1;34';
+    LightMagenta = '1;35';
+    LightCyan = '1;36';
+    LightWhite = '1;37';
+}
