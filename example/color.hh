207e46e884a35b58580d55b50cebf5ade36aa7f2
holyshared <holy.shared.design@gmail.com>
2016-01-22 12:08:57 +0900

    wrap function

--- example/color.hh
+++ example/color.hh
@@ -9,23 +9,27 @@ use hhpack\color\ForegroundColor;
 use hhpack\color\BackgroundColor;
 use hhpack\color\StyleType;
 
-foreach (ForegroundColor::getValues() as $name => $color) {
-    Color::fromColor($color)->display($name);
+function color_main() : void
+{
+    foreach (ForegroundColor::getValues() as $name => $color) {
+        Color::fromColor($color)->display($name);
+    }
+
+    foreach (BackgroundColor::getValues() as $name => $color) {
+        Color::fromBackground($color)->display($name);
+    }
+
+    Color::fromColor(ForegroundColor::White)
+        ->background(BackgroundColor::Green)
+        ->println('%s succeeded.', 'Color package example');
+
+    Color::fromColor(ForegroundColor::White)
+        ->addStyle(StyleType::Bold)
+        ->addStyle(StyleType::Underlined)
+        ->display('bold + underlined');
+
+    Color::fromDefault()
+        ->addStyle(StyleType::Reverse)
+        ->display('reverse');
 }
-
-foreach (BackgroundColor::getValues() as $name => $color) {
-    Color::fromBackground($color)->display($name);
-}
-
-Color::fromColor(ForegroundColor::White)
-    ->background(BackgroundColor::Green)
-    ->println('%s succeeded.', 'Color package example');
-
-Color::fromColor(ForegroundColor::White)
-    ->addStyle(StyleType::Bold)
-    ->addStyle(StyleType::Underlined)
-    ->display('bold + underlined');
-
-Color::fromDefault()
-    ->addStyle(StyleType::Reverse)
-    ->display('reverse');
+color_main();
